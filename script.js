d0cf5a3443bf4dbefbb2edcaa77f3937d5c0346f
= <=>
2023-03-03 13:00:26 +0530

    input

---
+++ script.js
@@ -0,0 +1,20 @@
+//Winning Pattern Array
+let winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  //Player 'X' plays first
+  let xTurn = true;
+  let count = 0;
+
+  /*button click rapeat*/
+  function myFunction() {
+    alert("The form was reset");
+  }
+
