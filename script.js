22b409c8a3319798efac8b147d0c7464160e0a95
= <=>
2023-03-04 17:27:20 +0530

    game loga

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
   //Player 'X' plays first
   let turn = "X";
+  let isgame = false;
  
 
  // function to change  the  turn
  const changeTurn = () => {
-    return turn === "x" ? "0": "X"
+    return turn === "X" ? "0": "X" ;
  }
+  
  //win
  const checkwin = () => {
-  let wins = [
+  let boxtext = document.getElementsByClassName('boxtext');
+  let win = [
   [0, 1, 2],
   [0, 3, 6],
   [2, 5, 8],
@@ -18,7 +21,15 @@
   [1, 4, 7],
   [0, 4, 8],
   [2, 4, 6],
-  ];
+  ]
+  win.forEach( e => { 
+   if (( boxtext[e[0]].value ===  boxtext[e[1]].value) && 
+    ( boxtext[e[2]].value ===  boxtext[e[1]].value) && ( boxtext[e[0]].value !== "")){
+    document.querySelector('.info').value = boxtext[e[0]].value + "Win"
+    isgame  = true
+    document.write("win")
+    }
+  })
 
  }
 
@@ -29,9 +40,22 @@
   element.addEventListener('click',()=>{
     if ( boxtext.value === '' ){
         boxtext.value = turn;
-       turn  = changeTurn ();
+       turn  = changeTurn();
       checkwin();
-      document.getElementsByClassName("info")[0].innerText  = "Turn for" + turn;
+      if(!isgame){
+      document.getElementsByClassName("info")[0].innerText  = "Turn for" + turn ;
+      }
     }
-  })
+  });
+ });
+
+ //reset
+ reset.addEventListener('click',()=>{
+  let boxtext = document.querySelectorAll('.boxtext');
+  Array.from(boxtext).forEach(element => {
+    element.value = ""
+  });
+  turn = "X" ;
+   
+  document.getElementsByClassName("info")[0].innerText  = "Turn for" + turn ;
  })
